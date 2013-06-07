2fb4c4a49fa34ad052c2243134aa44952e784d24
Yashodhan Tabib <tabiby@npg.wustl.edu>
2013-06-07 16:42:51 -0500

    Add Copyright and License notice to Java Files.

--- src/ImageAnnotation.bat
+++
@@ -1,9 +0,0 @@
-@ECHO %1
-@ECHO %2
-@ECHO %3
-@ECHO %4
-for /f %%i in ("%0") do set curpath=%%~dpi
-cd /d %curpath%
-java -Xms128m -Xmx256m -cp ..\lib\pixelmed.jar;..\lib\XIPApp.jar;..\lib\DicomUtil.jar;..\lib\mime-util.jar; ImageAnnotation %1 %2 %3 %4
-PAUSE
-REM EXIT
